4dea76928301d8e8c53d879c7ba9e03cc144b367
Olimpio Eduardo <kamuenhoeduardo@75gmail.com>
2025-08-12 00:43:40 +0100

    animate.css lib adicionado

--- src/components/events/index.tsx
+++ src/components/events/index.tsx
@@ -1,22 +1,35 @@
 'use client'
+import "animate.css"
+import { useInView } from "react-intersection-observer";
 
 import React from 'react';
 import Image from 'next/image';
-import { FadeInOnScrollProps } from '../FadeInScroll';
+
 
 export const Eventos = () => {
+
+   const {ref, inView} = useInView({
+    triggerOnce: true,
+    threshold:0.2
+  })
   return (
     <section id="eventos" className="flex flex-col items-center justify-center w-full pt-20 pb-14 px-4 sm:px-6 lg:px-10 xl:px-20 mt-10">
-      <FadeInOnScrollProps direction='up'> 
-      <h2 className="text-3xl sm:text-4xl font-bold text-center mb-6 text-[#026865]">
+    
+      <h2 
+      ref={ref}
+      className={`text-3xl sm:text-4xl font-bold text-center mb-6 text-[#026865] transition-opacity duration-700 ${
+        inView ? 'animate__animated animate__fadeInDownBig opacity-100': 'opacity-0'  }`}>
         Eventos
       </h2>
-      </FadeInOnScrollProps>
+      
       <div className="w-full max-w-screen-xl mx-auto">
         <div className="flex flex-col lg:flex-row items-center justify-center gap-8 mt-10">
           
-          <div className="flex items-center justify-center w-full max-w-md h-auto">
-            <FadeInOnScrollProps direction='left' > 
+          <div 
+          
+          className={`flex items-center justify-center w-full max-w-md h-auto ${
+        inView ? 'animate__animated animate__fadeInDownBig': ''  }`}>
+            
             <Image
               src="/assets/fipe.png"
               alt="FIPE"
@@ -26,19 +39,25 @@ export const Eventos = () => {
               loading="lazy"
               quality={100}
             />
-            </FadeInOnScrollProps>
+           
           </div>
 
           <div className="w-full max-w-2xl text-justify">
-            <FadeInOnScrollProps direction='right'>
-            <h5 className="text-[#026865] font-semibold text-lg mb-2">Sobre a FIPE</h5>
-            <p className="text-gray-800 text-sm sm:text-base leading-relaxed">
+            
+            <h5 className={`text-[#026865] font-semibold text-lg mb-2
+             transition-opacity duration-700 ${
+        inView ? 'animate__animated animate__fadeInDownBig opacity-100': 'opacity-0'  }`}>Sobre a FIPE</h5>
+            <p
+            ref={ref} 
+            className={`text-gray-800 text-sm sm:text-base leading-relaxed transition-opacity duration-700 ${
+        inView ? 'animate__animated animate__fadeInDownBig opacity-100': 'opacity-0'  }`}>
+
               <strong>A FIPE - Feira de Inteligência e Política Económica</strong>, é um evento de referência dedicado à promoção do conhecimento, inovação e diálogo estratégico nas áreas da inteligência económica, análise de dados e formulação de políticas públicas.
               Realizada anualmente, a FIPE reúne especialistas, decisores políticos, académicos, empresas e instituições, criando uma plataforma dinâmica para a partilha de ideias, experiências e soluções que impulsionam o desenvolvimento sustentável e a competitividade económica. <br /><br />
               O principal objetivo da FIPE é fomentar a cooperação entre os sectores público e privado, estimular o pensamento crítico e apresentar ferramentas e práticas inovadoras que orientem decisões estratégicas baseadas em evidências.
               A feira inclui conferências, workshops, exposições temáticas e sessões de networking, proporcionando um ambiente fértil para a troca de conhecimento e a criação de oportunidades.
             </p>
-             </FadeInOnScrollProps>
+           
           </div>
 
         </div>
--- src/components/partners/index.tsx
+++ src/components/partners/index.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import Image from 'next/image';
-import { FadeInOnScrollProps } from '../FadeInScroll';
 
 export const Partners = () => {
    return (
       <section id="parceiros" className="w-full flex flex-col items-center justify-center pt-20 pb-10 px-4 sm:px-6 lg:px-10 xl:px-20">
          <div className="flex flex-col justify-center items-center max-w-screen-xl mx-auto w-full">
-            <FadeInOnScrollProps direction='up'> 
+            
 
             <h2 className="text-3xl sm:text-4xl font-bold text-center mb-6 text-[#026865]">
                Parceiros
             </h2>
-</FadeInOnScrollProps>
+
             <p className="text-base sm:text-lg text-center mb-6 max-w-3xl text-gray-700">
                Conheça os nossos principais Parceiros de Negócios.
             </p>
 
             <div className="flex flex-wrap justify-center items-center gap-6 sm:gap-10 mt-10 w-full">
                <div className="flex items-center justify-center w-[120px] sm:w-[150px] h-[100px] sm:h-[150px]">
-                  <FadeInOnScrollProps direction='left'> 
+                 
                   <Image
                      src='/assets/aldwan.png'
                      alt='Aldwan Solutions'
@@ -30,11 +29,11 @@ export const Partners = () => {
                      quality={100}
                      style={{ objectFit: "cover" }}
                   />
-                  </FadeInOnScrollProps>
+                  
                </div>
                
                <div className="flex items-center justify-center w-[120px] sm:w-[150px] h-[100px] sm:h-[150px]">
-                  <FadeInOnScrollProps direction='right'> 
+                 
                   <Image
                      src='/assets/acacias-logo.png'
                      alt='Acácias Branding'
@@ -46,7 +45,7 @@ export const Partners = () => {
                      quality={100}
                      style={{ objectFit: "cover" }}
                   />
-                   </FadeInOnScrollProps>
+                   
                </div>
             </div>
          </div>
